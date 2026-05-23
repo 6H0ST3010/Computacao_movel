b26d2eb5fc9bc51d142cde13bc52b55102a3450c
6H0ST3010 <ramaral2003@gmail.com>
2026-05-23 17:33:47 +0100

    kotlin

---
+++ TP3/Kotlin/GreetingProcessorProject/app/src/main/kotlin/app/Main.kt
@@ -0,0 +1,15 @@
+package app
+
+fun main() {
+    val myClass = MyClass()
+    val wrappedMyClass = MyClassWrapper(myClass) // Use the wrapper class
+    wrappedMyClass.sayHello()
+    wrappedMyClass.compute()
+
+    println("Regex Annotation Processor")
+    val input = "Name: John Address: 123 Street"
+    val extractor = DataProcessorExtractor(input)
+    println("Name: ${extractor.getName()}")
+    println("Address: ${extractor.getAddress()}")
+
+}
